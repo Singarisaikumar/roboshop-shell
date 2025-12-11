ee3c03b6d96e5674f674539394707e3e8d075673
saikumar <wwwsaikumar150@gmail.com>
2025-12-11 23:32:57 +0530

    functions

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,7 +1,8 @@
 #!bin/bash
 
 instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
-
+domain_name="devopswithaws.store"
+hosted_zone_id="Z01037242PFYQFQ71R7F6"
 
 for name in ${instances[@]};do
     if [$name == "shipping" ] || [$name == "mysql" ]
@@ -11,4 +12,35 @@ for name in ${instances[@]};do
            instance_type="t3.micro"
     fi
     echo "Creating instances for: $name with instance type: $instance_type"
+    instance_id=$(aws ec2 run-instances  --image-id ami-09c813fb71547fc4f --instance-type $instance_type --security-group-ids sg-06507cfe4e2b0eaf2 --subnet-id subnet-0da0bf269651525a1 --query 'Instances[0].InstanceId' --output text)
+    echo "Instance created for: $name"
+
+    aws ec2 create-tags --resources $instance_id --tags Key=Name,Value=$name
+    
+    if [ $name == "web" ]
+    then
+        aws ec2 wait instance-running --instance-ids $instance_id
+        public_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        ip_to_use=$public_ip
+    else
+        private_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        ip_to_use=$private_ip
+    fi
+
+    aws route53 change-resource-record-sets --hosted-zone-id $hosted_zone_id --change-batch '
+   {
+  "Comment": "Creating a CNAME record for '$name'",
+  "Changes": [{
+      "Action"           : "UPSERT",
+      "ResourceRecordSet": {
+        "Name"           : "'$name.$domain_name'",
+        "Type"           : "A",
+        "TTL"            : 1,
+        "ResourceRecords": [
+          {
+            "Value"      : "'$ip_to_use'"
+          }]
+        }
+       }]
+    }'
 done
